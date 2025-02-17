f47674ca97c53ae78c67c7eaad747dcdb9706849
Sergio Herrera <chechoguz@gmail.com>
2025-02-16 23:19:49 -0300

    actualización para conectar backend

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000"; 
+// Usar la variable de entorno definida en .env
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Configurar Axios con la URL base
 const api = axios.create({
